f2976d7a90ebb2479eeb9e71860bcfa87ded63b2
Shonty266 <aryanhinglajia2663@gmail.com>
2024-10-16 21:32:40 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -48,10 +48,10 @@ app.get('/ping', (req, res) => {
 });
 
 // Serve static files from the frontend dist folder
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
 });
 
 app.use((req, res, next) => {
